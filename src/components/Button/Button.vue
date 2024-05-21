2df73b5f27b37da6fdc47ec5e74ec4b87a7bc042
chenruipeng <1259903760@qq.com>
2024-05-21 19:34:22 +0800

    feat: 项目初始化

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    class="vk-button"
+    :class="{
+      [`vk-button--${type}`]: type,
+      [`vk-button--${size}`]: size,
+      'is-plain': plain,
+      'is-round': round,
+      'is-circle': circle,
+      'is-disabled': disabled
+    }"
+    :disabled="disabled"
+  >
+    <span>
+      <slot />
+    </span>
+  </button>
+</template>
+
+<script setup lang="ts">
+import type { ButtonProps } from './types'
+
+defineOptions({
+  name: 'VkButton'
+})
+defineProps<ButtonProps>()
+</script>
+
+<style scoped>
+</style>
